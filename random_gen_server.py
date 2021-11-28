ff80b855f494cbd12469a02c0fd963e728f5009d
Aditya <adianand.blr@gmail.com>
2021-11-28 15:27:11 +0530

    changed random number generator server

--- random_gen_server.py
+++ random_gen_server.py
@@ -27,33 +27,45 @@ class RandomGenServer(object):
 
         self.id = int(sys.argv[1])
         self.family_id = int(sys.argv[2])
-        self.address = "127.0.0.1:" + str(sys.argv[3])
+        self.address = str(sys.argv[3])
         self.cloud_base_address = str(sys.argv[4])
         self.client_address = str(sys.argv[5])
 
         self.workers = dict()
 
+        localIP     = self.address.split(":")[0]
+        localPort   = int(self.address.split(":")[1]) #can be random but would be really convenient if we could just keep it same for all apps
+        self.bufferSize  = 1024
+
+        self.UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.UDPServerSocket.bind((localIP, localPort))
+
     def add_worker(self, msg):
+
+        print("ADDING A WORKER")
 
         worker = Worker()
         worker.id = msg.sender_id
         worker.address = msg.sender_address
-        worker.healthy = 1
-        worker.running = msg.status
+        worker.healthy = msg.status
+        worker.running = 0
         self.workers[worker.id] = worker
     
     def route_request(self, msg):
 
         available = [worker for worker in self.workers if self.workers[worker].running == 0 and self.workers[worker].healthy == 1]
+        print(self.workers[list(self.workers.keys())[0]].__dict__)
+        print(available)
 
         msg.sender_id = self.id
         if(not available):
             print("Dropping request")
             msg.type = "REQUEST_DROPPED"
             self.sendmsg(self.client_address, msg)
+            return
         decision = random.choice(available)
         msg.type = "USER_REQUEST"
-        self.sendmsg(self.workersk[decision].address, msg)
+        self.sendmsg(self.workers[decision].address, msg)
     
     def route_reply(self, msg):
 
@@ -69,7 +81,7 @@ class RandomGenServer(object):
 
         worker = self.workers[msg.sender_id]
         if(msg.type == "WORKER_HEALTH_UPDATE"):
-            worker.health = msg.status
+            worker.healthy = msg.status
         if(msg.type == "WORKER_ACTIVITY_UPDATE"):
             worker.running = msg.status
 
@@ -83,15 +95,16 @@ class RandomGenServer(object):
             
             current_time = time.time()
             for worker in self.workers:
+                #print(current_time, self.workers[worker].last_timestamp)
                 if(current_time - self.workers[worker].last_timestamp > 2):
-                    self.workers[worker].health = 0
+                    self.workers[worker].healthy = 0
 
             healthy_workers = [worker for worker in self.workers if self.workers[worker].healthy == 1]
             msg = Message()
             msg.sender_id = self.id
             msg.type = "PROCESS_HEALTH_UPDATE"
             msg.data = "Service " + str(self.family_id) + " has " + str(len(healthy_workers)) + " healthy processes out of " + str(len(list(self.workers)))
-            self.sendmsg(self.clientaddress, msg)
+            self.sendmsg(self.client_address, msg)
     
     def scale(self):
 
@@ -107,34 +120,32 @@ class RandomGenServer(object):
     
     
     def run(self):
-        localIP     = self.address.split(":")[0]
-        localPort   = int(self.address.split(":")[1]) #can be random but would be really convenient if we could just keep it same for all apps
-        bufferSize  = 1024
-
-        self.UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        self.UDPServerSocket.bind((localIP, localPort))
 
 
         while(True):
-            bytesAddressPair = self.UDPServerSocket.recvfrom(bufferSize)
+            print("about to read a message")
+            bytesAddressPair = self.UDPServerSocket.recvfrom(self.bufferSize)
+            print("just read a message")
             message = bytesAddressPair[0] #contains string form of object
             address = bytesAddressPair[1] #contains address of sender
             msg_obj = pickle.loads(message)
-            msg_obj.sender_address = bytesAddressPair[0] + ':' + str(bytesAddressPair[1])
+            msg_obj.sender_address = bytesAddressPair[1][0] + ":" + str(bytesAddressPair[1][1])
+
+            print(msg_obj.type)
 
             if(msg_obj.type == "CLIENT_REQUEST"):
                 self.route_request(msg_obj)
             elif(msg_obj.type == "WORKER_REPLY"):
                 self.route_reply(msg_obj)
-            elif(msg_obj.type == "ADD_WORKER"): #idk what else to put here
-                self.add_worker(msg_obj)
-            else:
+            elif(msg_obj.type == "WORKER_HEALTH_UPDATE" or msg_obj.type == "WORKER_ACTIVITY_UPDATE" ):
                 self.status_update(msg_obj)
 
     def sendmsg(self, address, msg):
         serial_msg = pickle.dumps(msg)
         address_tuple = (address.split(":")[0], int(address.split(":")[1]))
-        self.UDPServerSocket.sendto(address_tuple, serial_msg)
+        #print("sending message "+str(msg.__dict__)+" to address " + address)
+        print(str(address_tuple))
+        self.UDPServerSocket.sendto(serial_msg, address_tuple)
 
 if(__name__ == "__main__"):
     server = RandomGenServer()
